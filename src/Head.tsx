8fa0cebb15bcc42d59c2ee5c7d41bb62bbc8b8c4
kenokenobingo <mail.kewe@gmail.com>
2021-04-07 11:44:14 +0200

    Design changes: fonts, colors, title

--- src/Head.tsx
+++ src/Head.tsx
@@ -3,7 +3,7 @@ import {Helmet} from 'react-helmet';
 export default function Head() {
   return (
     <Helmet>
-      <title>MOLOCH v3</title>
+      <title>compost.energy</title>
       <meta
         name="description"
         content="A modular DAO framework developed and coordinated by its members"
