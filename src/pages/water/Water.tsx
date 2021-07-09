3fddb3748de6f0d06d41a92bfd24dbd41be89837
kenokenobingo <mail.kewe@gmail.com>
2021-07-09 11:11:45 +0200

    Created route to water page

---
+++ src/pages/water/Water.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+import {DaoAdapterConstants} from '../../components/adapters-extensions/enums';
+import FadeIn from '../../components/common/FadeIn';
+import Proposals from '../../components/proposals/Proposals';
+import Wrap from '../../components/common/Wrap';
+
+export default function Water() {
+  /**
+   * Render
+   */
+
+  return (
+    <RenderWrapper>
+      <Proposals
+        adapterName={DaoAdapterConstants.TRIBUTE}
+        includeProposalsExistingOnlyOffchain={true}
+      />
+    </RenderWrapper>
+  );
+}
+
+function RenderWrapper(props: React.PropsWithChildren<any>): JSX.Element {
+  /**
+   * Their hooks
+   */
+
+  const history = useHistory();
+
+  /**
+   * Functions
+   */
+
+  function triggerIrrigation(event: React.MouseEvent<HTMLButtonElement>) {
+    
+  }
+
+  /**
+   * Render
+   */
+
+  return (
+    <Wrap className="section-wrapper">
+      <FadeIn>
+        <div className="titlebar">
+          <h2 className="titlebar__title">Water</h2>
+          <button className="titlebar__action" onClick={triggerIrrigation}>
+            Triggier Irrigation
+          </button>
+        </div>
+
+        {/* RENDER CHILDREN */}
+        {props.children}
+      </FadeIn>
+    </Wrap>
+  );
+}
